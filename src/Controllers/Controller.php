370388cd629c4baa550dd9fd1391a4bab2bc19f8
Denis Kipkemboi <deniskem.dev@gmail.com>
2023-02-03 20:07:44 +0300

    updated namespace

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Controllers;
+namespace Denniskemboi\LaravelRestHelper\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
